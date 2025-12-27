8ae1b4812113247ae9a18edb21095ee8aa715134
Rahul <rahulcherukuwada.com>
2025-12-27 21:45:59 +0530

    Update settings for deployment

--- gym_backend/settings.py
+++ gym_backend/settings.py
@@ -1,8 +1,11 @@
 """
 Django settings for gym_backend project.
 """
+import os
+import dj_database_url
 
 from pathlib import Path
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,11 +91,18 @@ TEMPLATES = [
 # =========================
 # DATABASE
 # =========================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
